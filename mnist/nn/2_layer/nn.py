cd507b220f3e1ab8df5085da166c67e8cb5028b2
Blurgy <gaoyangzhang@outlook.com>
2019-08-13 01:55:11 +0800

    :sparkles: added bias:

--- mnist/nn/2_layer/nn.py
+++ mnist/nn/2_layer/nn.py
@@ -29,18 +29,20 @@ class ReLU(gate):
 class multiply_gate(gate):
     def __init__(self, ):
         super().__init__();
-    def forward(self, w, x, ):
+    def forward(self, w, x, b, ):
         self.w = w;
         self.x = x;
-        self.z = np.dot(self.w, self.x);
+        self.b = b;
+        self.z = np.dot(self.w, self.x) + self.b;
         return self.z;
     def backward(self, dz, ):
         # @return dW and dx;
         self.dw = np.dot(dz, self.x.T);
         self.dx = np.dot(self.w.T, dz);
+        self.db = dz;
         # print(dz);
         # print(self.dw.shape, self.dx.shape);
-        return [self.dw, self.dx];
+        return [self.dw, self.dx, self.db];
 
 def decay_schedule(length, name):
     i = np.arange(0, length, 1);
@@ -69,10 +71,15 @@ def init_model(input_size, hidden_layer_size, output_size, reg_strength):
     model['w2.m'] = np.zeros(model['w2'].shape); # Adam update
     model['w2.v'] = np.zeros(model['w2'].shape); # Adam update
     model['layer_2'] = multiply_gate();
-    model['score'] = None;
+    model['output'] = None;
     model['iter_cnt'] = 0;
     model['w1.grad'] = np.zeros(model['w1'].shape);
     model['w2.grad'] = np.zeros(model['w2'].shape);
+
+    model['b1'] = np.random.randn(hidden_layer_size, 1);
+    model['b2'] = np.random.randn(output_size, 1);
+    model['b1.grad'] = np.zeros(model['b1'].shape);
+    model['b2.grad'] = np.zeros(model['b2'].shape);
     return model;
 
 def sample_batches(training_set, batch_size):
@@ -89,9 +96,9 @@ def sample_batches(training_set, batch_size):
 
 def forward(model, img, is_test_time):
     model['input'] = img;
-    model['h1'] = model['layer_1'].forward(model['w1'], model['input']);
+    model['h1'] = model['layer_1'].forward(model['w1'], model['input'], model['b1']);
     model['h1'] = model['ReLU'].forward(model['h1'], is_test_time);
-    model['score'] = model['layer_2'].forward(model['w2'], model['h1']);
+    model['score'] = model['layer_2'].forward(model['w2'], model['h1'], model['b2']);
 
 def update_weights(model, learning_rate):
     model['w1'] += model['reg_strength'] * learning_rate * model['w1'];
@@ -101,12 +108,19 @@ def update_weights(model, learning_rate):
     model['w1.grad'] *= 0;
     model['w2.grad'] *= 0;
 
+    model['b1'] += -learning_rate * model['b1.grad'];
+    model['b2'] += -learning_rate * model['b2.grad'];
+    model['b1.grad'] *= 0;
+    model['b2.grad'] *= 0;
+
 def sgd_backward(model, dz, batch_size):
     model['layer_2'].backward(dz);
     model['w2.grad'] += model['layer_2'].dw / batch_size;
+    model['b2.grad'] += model['layer_2'].db / batch_size;
     model['ReLU'].backward(model['layer_2'].dx);
     model['layer_1'].backward(model['ReLU'].dx);
     model['w1.grad'] += model['layer_1'].dw / batch_size;
+    model['b1.grad'] += model['layer_1'].db / batch_size;
 
 def adam_backward(model, dz, learning_rate):
     model['iter_cnt'] += 1;

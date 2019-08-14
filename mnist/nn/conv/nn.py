0d47d3319f14f2edf7fcba0f8f50edbd02d771de
Blurgy <gaoyangzhang@outlook.com>
2019-08-15 01:25:16 +0800

    :poop: writing :poop: code, will rework tomorrow (current code cannot run)

--- mnist/nn/conv/nn.py
+++ mnist/nn/conv/nn.py
@@ -4,39 +4,54 @@ __author__ = "Blurgy";
 
 import numpy as np
 
-def conv(x, f, s = 1):
-    # x: input;
-    # f: filter
-    # s: stride
-    # requires x and f to have same depth
-    depth, f_wid, f_hei = f.shape;
+def im2col(batch, f_size, padding, stride, ):
+    # batch: n * d * h * w
+    # stride = stride;
+    # f_size = f_size;
+    n, d, h, w = batch.shape;
+    p = padding;
+    batch_pad = np.pad(batch, ((0,0), (0,0), (p,p), (p,p)));
     ret = [];
-    row = [];
-    for ver in range(0, x.shape[1]-f_hei+1, s):
-        for hor in range(0, x.shape[2]-f_wid+1, s):
-            row.append(np.sum(x[:, ver:ver+f_wid, hor:hor+f_hei] * f));
-        ret.append(row);
-        row = [];
+    for x in batch_pad:
+        elem = None;
+        for ver in range(0, h+p*2-f_size+1, stride):
+            for hor in range(0, w+p*2-f_size+1, stride):
+                # print(x[:, ver:ver+f_size, hor:hor+f_size]);
+                # print(x[:, ver:ver+f_size, hor:hor+f_size].reshape(f_size*f_size*d, 1));
+                # input();
+                column = np.reshape(x[:, ver:ver+f_size, hor:hor+f_size], (f_size*f_size*d, 1));
+                if(type(elem).__name__ == "NoneType"):
+                    elem = column;
+                else:
+                    elem = np.hstack((elem, column));
+        ret.append(elem);
     ret = np.array(ret);
+    # print("im2col batch.shape", batch.shape)
+    # print("im2col:\n", ret, ret.shape)
     return ret;
 
-# class fltr:
-#     def __init__(self, size, depth, ):
-#         self.size = size;
-#         self.depth = depth;
-#         self.init_weights();
-#     def init_weights(self, ):
-#         self.f = 0.001 * np.random.randn(self.depth, self.size, self.size);
+def col2im(batch, h, w):
+    n, k, h_w = batch.shape;
+    ret = [];
+    for x in batch:
+        elem = [];
+        for i in range(k):
+            elem.append(np.array(x[i, :]));
+        ret.append(np.array(elem).reshape(k, h, w));
+    ret = np.array(ret).reshape(n, k, h, w);
+    print("col2im batch.shape:", batch.shape);
+    print("col2im", ret.shape)
+    return ret;
 
 def fltr(size, depth):
-    return 0.01 * np.random.randn(depth, size, size);
+    return 0.01 * np.random.randn(depth*size*size, 1);
 
 class conv_layer:
-    def __init__(self, k_filters, f_size, f_depth, stride, padding, ):
+    def __init__(self, k_filters, f_size, f_depth, stride, padding):
         # k: number of filters
         # f: filters' spatial extent
-        # stride: stride
-        # padding: amount of zero padding
+        # stride = 1
+        # padding = 1
         self.k_filters = k_filters;
         self.f_size = f_size;
         self.f_depth = f_depth;
@@ -45,41 +60,83 @@ class conv_layer:
         self.filters = [];
         # self.bias = 
         self.init_filters();
-        self.df = [];
     def init_filters(self, ):
         for i in range(self.k_filters):
             self.filters.append(fltr(size = self.f_size, depth = self.f_depth));
+        self.filters = np.array(self.filters).reshape(self.k_filters, self.f_size*self.f_size*self.f_depth);
+        self.df = np.zeros(self.filters.shape);
     def forward(self, x, ):
-        # x: w1 * h1 * d1
-        # do padding in advance
-        self.x = np.pad(x, ((0,0), (self.padding, self.padding), (self.padding, self.padding)));
-        self.z = [];
-        for f in self.filters:
-            elem = conv(self.x, f, self.stride)
-            self.z.append(elem);
-        self.z = np.array(self.z);
+        # x: n * d * h * w
+        self.x = x;
+        n, d, h, w = self.x.shape;
+        output_h = int((h + 2*self.padding - self.f_size) / self.stride + 1);
+        output_w = int((w + 2*self.padding - self.f_size) / self.stride + 1);
+        # x_reshaped = self.x.reshape(n*d, 1, h, w);
+        self.x_col = im2col(self.x, f_size = self.f_size, padding = self.padding, stride = self.stride);
+        self.z_col = [];
+        for b in range(n):
+            self.z_col.append(self.filters @ self.x_col[b]);
+        self.z_col = np.array(self.z_col);
+        self.z = col2im(self.z_col, output_h, output_w);
         return self.z;
     def backward(self, dz, ):
-        # print(dz.shape);
-        if(self.df == []):
-            for i in range(0, self.k_filters):
-                self.df.append(np.zeros((self.f_depth, self.f_size, self.f_size)));
+        # dz: n * k * h * w
+        self.dz = dz;
+        n, k, h, w = self.dz.shape;
+        output_h = (h-1) * self.stride + self.f_size - 2*self.padding;
+        output_w = (w-1) * self.stride + self.f_size - 2*self.padding;
+        dz_reshaped = self.dz.reshape(n*k, 1, h, w);
+        self.dz_col = im2col(dz_reshaped, f_size = self.f_size, padding = self.padding, stride = self.stride);
         self.dx = np.zeros(self.x.shape);
-        for i in range(0, dz.shape[0]):
-            # for f in self.filters:
-            for j in range(self.k_filters):
-                # print(self.dx.shape);
-                for ver in range(0, self.dx.shape[1]-self.f_size+1, self.stride):
-                    for hor in range(0, self.dx.shape[2]-self.f_size+1, self.stride):
-                        self.dx[:, ver:ver+self.f_size, hor:hor+self.f_size] += self.filters[j] * dz[i,int(ver/self.stride),int(hor/self.stride)];
-                        self.df[j] += self.x[:, ver:ver+self.f_size, hor:hor+self.f_size] * dz[i,int(ver/self.stride),int(hor/self.stride)];
-        self.dx = self.dx[:, self.padding:self.dx.shape[1]-self.padding, self.padding:self.dx.shape[2]-self.padding];
-        self.db = dz;
+        self.dx_col = im2col(self.dx, f_size = self.f_size, padding = self.padding, stride = self.stride);
+        """ caclulate df """
+        for l in range(n*k):
+            for i in range(h*w):
+                self.df[l%k,:] += (self.dz_col[l,:,i] * self.x_col[int(l/k),:,i]);
+        """ caclulate dx """
+        print("self.x.shape", self.x.shape)
+        print("self.dx_col.shape", self.dx_col.shape)
+        print("self.dz_col.shape", self.dz_col.shape)
+        print("self.filters.shape", self.filters.shape)
+        for l in range(n*k):
+            for i in range(h*w):
+                self.dx_col[int(l/k),:,i] += self.dz_col[l,:,i] * self.filters[l%k,:];
+                pass;
+        self.dx = col2im(self.dx_col, output_h, output_w);
+
+        # for b in range(n):
+        #     for l in range(k):
+        #         for i in range(h):
+        #             for j in range(w):
+        #                 self.dx[b, ] += self.dz[b][l][i][j] 
+        # self.dx = np.dot(self.filters.reshape(1,-1), self.dz_col).reshape(self.x.shape);
+
+        # self.df = []
+        # for i in range(n):
+        #     print(self.dz[i].reshape(self.k_filters, h*w));
+        #     print(self.x_col[i].T);
+        #     self.df.append(np.dot(self.dz[i].reshape(self.k_filters, h*w), self.x_col[i].T));
+        # self.df = np.array(self.df);
+        # # print(self.filters.shape)
+        # # print(self.df.shape)
+        # self.db = dz;
         return self.dx;
     def update(self, learning_rate, ):
-        for i in range(self.k_filters):
-            self.filters[i] += -learning_rate * self.df[i];
-        self.df = [];
+        self.filters += -learning_rate * self.df;
+        self.df = 0;
+
+class pooling_layer:
+    def __init__(self, size, stride, ):
+        self.size = size;
+        self.stride = stride;
+        self.max_idx = [];
+    def forward(self, x, ):
+        self.z = [];
+        d, h, w = x.shape;
+
+
+        return self.z;
+
 
 class fc_layer:
     def __init__(self, input_size, output_size):
@@ -98,12 +155,12 @@ class fc_layer:
     def forward(self, x, ):
         self.x = x;
         # print(self.w.shape, self.x.shape);
-        self.z = np.dot(self.w, self.x) + self.b;
+        self.z = self.w @ self.x + self.b;
         return self.z;
     def backward(self, dz, ):
-        self.dw += np.dot(dz, self.x.T);
+        self.dw += dz @ self.x.T;
         self.db += dz;
-        self.dx = np.dot(self.w.T, dz);
+        self.dx = self.w.T @ dz;
         return self.dx;
     def update(self, learning_rate, ):
         self.w += -learning_rate * self.dw;
@@ -157,19 +214,13 @@ def init_model():
     model = {};
     model['input'] = None;
     model['conv1'] = conv_layer(k_filters = 4,
-                                f_size = 3, f_depth = 1,
-                                stride = 1, padding = 1);
+                                f_size = 3, f_depth = 1);
     model['relu1'] = ReLU();
-    model['pooling1'] = conv_layer(k_filters = 4,
-                                   f_size = 2, f_depth = 4,
-                                   stride = 2, padding = 0);
+    # model['pooling1'] = 
     model['conv2'] = conv_layer(k_filters = 16,
-                                f_size = 3, f_depth = 4,
-                                stride = 1, padding = 1);
+                                f_size = 3, f_depth = 4);
     model['relu2'] = ReLU();
-    model['pooling2'] = conv_layer(k_filters = 16,
-                                   f_size = 2, f_depth = 16,
-                                   stride = 2, padding = 0);
+    # model['pooling2'] = 
     model['fc6'] = fc_layer(input_size = 784, output_size = 200);
     model['relu3'] = ReLU();
     model['fc7'] = fc_layer(input_size = 200, output_size = 10);

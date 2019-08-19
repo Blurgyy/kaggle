3ceb0cccafb5f51e364ff326a8e2f0767b0c8291
Blurgy <gaoyangzhang@outlook.com>
2019-08-19 15:17:30 +0800

    :construction: default lr changed to 1e-3

--- mnist/nn/conv/train.py
+++ mnist/nn/conv/train.py
@@ -15,8 +15,8 @@ warnings.filterwarnings("error")
 @click.command()
 @click.option("--epoch", type = int, default = 10, 
               help = "Specifies number of epoches, 10 by default")
-@click.option("--rate", type = float, default = 1e-2, 
-              help = "Specifies value of initial learning rate, 1e-2 by default")
+@click.option("--rate", type = float, default = 1e-3, 
+              help = "Specifies value of initial learning rate, 1e-3 by default")
 @click.option("--decay", type = click.Choice(["exponential", "constant", "linear", "sigmoid", "hyperbola"]), 
               default = "constant", 
               help = "Specifies decay schedule of learning rate, constant by default")
